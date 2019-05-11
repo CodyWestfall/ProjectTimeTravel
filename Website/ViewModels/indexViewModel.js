d2aedf65ae0c484cf69ff6be1ba567e40108a598
CodyWestfall <codywestfall@oakland.edu>
2019-05-11 06:43:03 -0400

    create base website

---
+++ Website/ViewModels/indexViewModel.js
@@ -0,0 +1,17 @@
+//the view model
+var indexViewModel = function() {
+    self = this;
+
+    //called to redraw the map
+    initialize = function () {
+
+    }
+
+
+    //initialize
+    initialize();
+
+};
+
+//binding the viewmodel to the view
+ko.applyBindings(new indexViewModel());
